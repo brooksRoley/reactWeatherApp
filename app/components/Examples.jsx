b21176f474846ecef423b444d01ad65c2d065dcc
Brooks Roley <brooksroley@gmail.com>
2017-09-22 11:24:14 -0700

    heroku push changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,15 +1,5 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-// 	render: function(){
-// 		return(
-// 			<div>
-// 				<h2>Examples Component</h2>
-// 			</div>
-// 		);
-// 	}
-// })
-
 var Examples = (props) => {
 	return(
 		<div>
