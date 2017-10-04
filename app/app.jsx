559ee637b4fff5bf311adfbe2de768c016644aec
zero400 <brooksroley@gmail.com>
2017-10-04 11:03:55 -0700

    Adding foundation to style application

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
 // Sets each variable to the object property from the 'react-router'.
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
+
+// load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 ReactDOM.render(
 	<Router history={hashHistory}>
